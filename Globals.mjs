cb02d7daba2063c5b9be5c62ac29aebf03c9fcae
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-09-25 23:33:20 +0700

    penambahan GLoblas dalam pelatihan NodeJS

---
+++ Globals.mjs
@@ -0,0 +1,9 @@
+// function yang bisa langsung di pakai tanpa mengimport dari nodejs
+
+// di dalam node js, terdapt library berupa variable atau function yang secara global bisa di akses dimana saja, tanpa harus melakukan import
+// kita bisa melihat detail apa saja fitur yang terdapat secara global di halaman dokumentasinya
+
+setTimeout(() => {
+    console.info("Hello Globals");
+
+}, 2000);
