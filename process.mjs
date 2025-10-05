64a8d38e792b86d701aace36538c834b47967c81
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-10-05 23:57:18 +0700

    latihan nodejs process

--- process.mjs
+++ process.mjs
@@ -6,3 +6,10 @@ import process from "process";
 process.addListener("exit", (exitCode)=>{
     console.info(`Node.JS exit with code  ${exitCode}`);
 });
+
+console.info(process.version);
+console.info(process.argv);
+console.info(process.report);
+
+process.exit(1);
+console.info(process.env);
